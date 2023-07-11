b88be1deef7db12165ca9f998e985a2f18aebace
Tinh Nguyen <tinh258456@gmail.com>
2023-07-11 15:44:09 +0700

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const splitButton = document.querySelector("#split-btn");
 const leftTab = document.querySelector("#split-left");
 
 splitButton.addEventListener("click", onSplitBtnClick)
-
+console.log(`splitButton`,splitButton)
 function onSplitBtnClick () {
   if (leftTab.className.includes('list-split'))
   leftTab.className = "hidden-aside left-panel"
@@ -13,20 +13,35 @@ function onSplitBtnClick () {
 //View all btn comment
 const DeskComment = document.querySelector("#desk-comment")
 
-const allComments =  DeskComment.querySelectorAll("[data-type=comment]");
-const viewAllBtn =  DeskComment.querySelector(".view-all-btn")
+const allComments =  DeskComment.querySelectorAll("[data-key=comment]");
+const viewAllBtn =  document.querySelector("#view-all-btn-comment")
+console.log(`~~~~ allComments`,allComments.length)
+
+console.log(`~~~~ viewAllBtn`,viewAllBtn)
     if (allComments.length > 1){
      viewAllBtn.style.visibility = "visible";
-     viewAllBtn.addEventListener("click", onViewAllClick())
     }
+viewAllBtn.addEventListener("click", onViewAllCommentClick)
 
 function onViewAllCommentClick () {
+  console.log(`open~~~`)
+
   if (viewAllBtn.className.includes('hide'))
   {
-    allComments.map((comment,index)=>{ if(index > 0) comment.style.visibility ="hidden" })
+    Array.from(allComments).map((comment,index)=>{
+       if(index > 0) comment.setAttribute("hidden",true)
+      })
+      console.log(`open~~~`)
+
+      viewAllBtn.className = "view-all-btn"
+      viewAllBtn.innerHTML = "View All"
+
   }
   else {  
-    allComments.map((comment,index)=>{ if(index > 0) comment.style.visibility ="visible" })
-    viewAllBtn.className = "view-all-btn hide"
+    Array.from(allComments).map((comment,index)=>{ 
+      if(index > 0) comment.removeAttribute("hidden") })
+      viewAllBtn.className = "view-all-btn hide"
+      viewAllBtn.innerHTML = "Hide"
+      console.log(`close~~~`)
 }
 }
