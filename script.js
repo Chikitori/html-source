c8b53223a41e2436c82770ad2f13a78f59a91a91
Tinh Nguyen <tinh258456@gmail.com>
2023-07-11 15:00:45 +0700

    update script

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function onSplitBtnClick () {
   leftTab.className = "hidden-aside left-panel"
   else   leftTab.className = "list-split left-panel"
 }
+
+
+//View all btn comment
+const DeskComment = document.querySelector("#desk-comment")
+
+const allComments =  DeskComment.querySelectorAll(`[data-type=comment]`);
+const viewAllBtn =  DeskComment.querySelector(".view-all-btn")
+    if (allComments.length > 1){
+     viewAllBtn.style.visibility = "visible";
+     viewAllBtn.addEventListener("click", onViewAllClick())
+    }
+
+function onViewAllCommentClick () {
+  if (viewAllBtn.className.includes('hide'))
+  {
+    allComments.map((comment,index)=>{ if(index > 0) comment.style.visibility ="hidden" })
+  }
+  else {  
+    allComments.map((comment,index)=>{ if(index > 0) comment.style.visibility ="visible" })
+    viewAllBtn.className = "view-all-btn hide"
+}
+}
